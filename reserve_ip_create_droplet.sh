49f5995522cfaad710330e54854cf872908d358d
Krzysztof_Waclawski_SF57748 <krzysztof.waclawski@stellantis.external.com>
2025-01-22 16:33:15 +0100

    refactor to automate reerved ip creation

--- reserve_ip_create_droplet.sh
+++ reserve_ip_create_droplet.sh
@@ -3,6 +3,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Reserve a new IP
+RESERVED_IP=$(doctl compute reserved-ip create --region nyc3 --format IP --no-header)
+
 # Create a new Droplet
 DROPLET_ID=$(doctl compute droplet create build-server \
   --region nyc3 \
@@ -17,3 +20,6 @@ DROPLET_ID=$(doctl compute droplet create build-server \
 doctl compute reserved-ip-action assign $RESERVED_IP --droplet-id $DROPLET_ID
 
 echo "Droplet created and assigned reserved IP: $RESERVED_IP"
+
+# Save the reserved IP to a file for later use
+echo $RESERVED_IP > reserved_ip.txt
